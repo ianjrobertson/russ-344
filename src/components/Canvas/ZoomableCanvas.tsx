179124f41c1871ba05bb17d0cbe527d315623ac9
Ian <ijr011235@gmail.com>
2025-12-07 21:02:47 -0700

    fix type

--- src/components/Canvas/ZoomableCanvas.tsx
+++ src/components/Canvas/ZoomableCanvas.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import type { CanvasConfig } from '../../types';
 
